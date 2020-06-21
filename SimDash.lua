3c9d4d340b901ac93023b6da585c0a0d86b4d3c4
Sterling Makara <40364940+STERFRY333@users.noreply.github.com>
2020-06-20 18:59:15 -0700

    Version 1.0

---
+++ SimDash.lua
@@ -0,0 +1,77 @@
+local M = {}
+
+local engtime = 0
+local engtimer = 0
+local particleAmount = 0
+local ignkeySmoother = newExponentialSmoothing(6)
+local oilPressureSmoother = newExponentialSmoothing(80)
+local voltsSmoother = newExponentialSmoothing(5)
+local fuelaltSmoother = newExponentialSmoothing(120)
+local tempaltSmoother = newExponentialSmoothing(120)
+
+
+local function init()
+   electrics.values.shiftlight = 0
+end
+
+local function reset()
+   init()
+end
+
+local function updateGFX(dt)
+   --IGNITION
+   
+  if powertrain.getDevice("mainEngine").ignitionCoef ~= 0 and electrics.values.rpm > 100 then electrics.values.ignkey = 2
+  else electrics.values.ignkey = 0 end
+  
+	if electrics.values.rpm < 100 and powertrain.getDevice("mainEngine").ignitionCoef ~= 0 then electrics.values.ignkey = 1 end
+ 
+ 
+  
+    electrics.values.ignkey = ignkeySmoother:get(electrics.values['ignkey'])
+
+	--OIL PRESSURE
+   
+    if electrics.values.rpm > 500 and powertrain.getDevice("mainEngine").oilVolume > 2.5 then electrics.values.oilPressure = electrics.values.rpm  *0.0001000 * powertrain.getDevice("mainEngine").oilVolume / 0.05 + 55
+	else electrics.values.oilPressure = electrics.values.rpm  *0.2 * powertrain.getDevice("mainEngine").oilVolume / 12.5 end
+	
+
+	 electrics.values.oilPressure = oilPressureSmoother:get(electrics.values['oilPressure'])
+ 
+	 
+	 --ALTERNATE FUEL
+	 
+    if electrics.values.ignkey < 1 then electrics.values.fuelalt = 0 end 
+	electrics.values.fuelalt = fuelaltSmoother:get(electrics.values['fuelBase'])
+	
+	--VOLTS
+	
+    if electrics.values.ignkey > 1.5 then electrics.values.volts = 40
+	else electrics.values.volts = 5 end
+	if electrics.values.ignkey < 0.5 then electrics.values.volts = -45 end
+	if powertrain.getDevice("mainEngine").starterEngagedCoef > 0 then electrics.values.volts = -10 end
+	
+	
+	electrics.values.volts = voltsSmoother:get(electrics.values['volts'])
+	
+
+	if electrics.values.oilPressure < 35 and electrics.values.ignkey > 0.5  then electrics.values.oillow = 1 else electrics.values.oillow = 0 end
+	
+	if electrics.values.volts < 0 and electrics.values.ignkey > 0.5  then electrics.values.battery = 1 else electrics.values.battery = 0 end
+	
+	if electrics.values.ignkey == 1 then electrics.values.checkengine = 1 end
+
+	
+
+	
+ 
+	
+end
+
+
+
+M.onInit = init
+M.onReset = init
+M.updateGFX = updateGFX
+
+return M
